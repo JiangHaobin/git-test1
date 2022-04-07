1b2503b7582cf128a9c69b9d3e060703d2189551
jhb <736624781@qq.com>
2022-04-07 13:48:06 +0800

    first commit master

---
+++ src/main/java/com/atguigu/hello.java
@@ -0,0 +1,18 @@
+package com.atguigu;
+
+/**
+ * @projectName: git-test
+ * @package: com.atguigu
+ * @className: hello
+ * @author: jhb
+ * @description: TODO
+ * @date: 2022/4/7 13:47
+ * @version: 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello.........");
+        System.out.println("hello.........");
+        System.out.println("hello.........");
+    }
+}
