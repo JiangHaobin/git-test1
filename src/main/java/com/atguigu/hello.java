f006f7f70a80f0472b6f3bd7372d323ac5519cba
jhb <736624781@qq.com>
2022-04-07 13:48:55 +0800

    second commit master

--- src/main/java/com/atguigu/hello.java
+++ src/main/java/com/atguigu/hello.java
@@ -14,5 +14,7 @@ public class hello {
         System.out.println("hello.........");
         System.out.println("hello.........");
         System.out.println("hello.........");
+        System.out.println("masterhello.........");
+
     }
 }
